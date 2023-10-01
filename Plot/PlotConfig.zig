9deb25f12a52461ea28cbc0587ce2c6fc4c4db9d
Adrian <arrodriguez1809@gmail.com>
2023-10-01 20:32:56 +0200

    Typo fixed

--- Plot/PlotConfig.zig
+++ Plot/PlotConfig.zig
@@ -1,19 +1,11 @@
 const std = @import("std");
 
-pub const OutputMode = enum { 
-    PNG,
-    PNG_CAIRO,
-    QT,
-    X11,
-    Default
-};
+pub const OutputMode = enum { PNG, PNG_CAIRO, QT, X11, Default };
 
 pub const PlotConfig = struct {
-
-
     output_mode: OutputMode,
     name: ?[]const u8 = null,
-    sepatator: ?[]const u8 = null,
+    separator: ?[]const u8 = null,
 
     grid: bool = false,
 
